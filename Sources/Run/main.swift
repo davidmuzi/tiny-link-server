2ca27ef781c7143686ffa69a0d06b79c5128ef4a
David Muzi <david.muzi@shopify.com>
2018-04-30 22:41:43 -0400

    seed random generator

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -5,6 +5,10 @@ import Foundation
 
 // The contents of main are wrapped in a do/catch block because any errors that get raised to the top level will crash Xcode
 do {
+	#if os(Linux)
+	srandom(UInt32(time(nil)))
+	#endif
+	
     var config = Config.default()
     var env = try Environment.detect()
     var services = Services.default()
